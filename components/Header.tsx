2f358f4d9e54d763b6011b83fbad586a68f333ff
Johnny Nguyen <56814739+nddung2812@users.noreply.github.com>
2023-10-08 22:27:06 +1000

    add QR code

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,6 @@ export default function Header() {
       <a
         className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
         href="/connect"
-        target="_blank"
         rel="noopener noreferrer"
       >
         <FaPaperPlane />
